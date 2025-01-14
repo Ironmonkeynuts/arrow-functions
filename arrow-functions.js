87c916dddfbbbe2d55c52665a51cf7384540d22c
David Doyle-Owen <thinkimagine@outlook.com>
2025-01-14 14:48:35 +0000

    Add code to demonstrate arrow function

--- arrow-functions.js
+++ arrow-functions.js
@@ -5,9 +5,26 @@
 
 // Vanilla JavaScript Function
 
+function addTwoNumbers (a, b){
+    return a + b;
+}
+let sum = addTwoNumbers (3, 5)
+console.log(sum);
+
+
 // Arrow Function With Parameters
 
+const addTwoNumbers2 = (a, b) => {
+    return a + b;
+}
+let sum2 = addTwoNumbers2 (3, 5)
+console.log(sum2);
+
 // Single Line Arrow Function With Parameters
+
+const addTwoNumbers3 = (a, b) =>  a + b;
+let sum3 = addTwoNumbers3 (3, 5)
+console.log(sum3);
 
 // Implicit Returns
 
